ad6c05d0a64f2ec09df050f7735e5830dc24f9d6
Matteo <matteozaratti@gmail.com>
2020-01-25 16:08:49 +0000

    Fix API server

--- Backend/src/config/index.js
+++ Backend/src/config/index.js
@@ -5,7 +5,7 @@ const baseConfig = {
   env,
   isDev: env === 'development',
   isTest: env === 'testing',
-  port: 8000,
+  port: 3000,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: '100d'
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -63,9 +63,7 @@ server.listen(WS_PORT, () => {
 export const start = async () => {
   try {
     await connect()
-    app.listen(config.port, () => {
-      console.log(`REST API on http://localhost:${config.port}/api`)
-    })
+    app.listen(3000, () => console.log("Running on localhost: 3000"))
   } catch (e) {
     console.error(e)
   }
